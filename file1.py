c1befa20ef81a6200e48ef37a7ac4f9ae27c1a78
soumya-sunkad <soumyasunkad06@gmail.com>
2025-10-18 11:05:32 +0530

    save

---
+++ file1.py
@@ -0,0 +1,6 @@
+temp=int(input("Enter the temperature in degree celsius:"))
+fahrenheit=(temp*9/5)+32
+print("The temperature in degree celsius:",temp)
+print("Converting degree celsius to the fahrenheit:",fahrenheit)
+
+
