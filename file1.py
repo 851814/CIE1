093f8d0b2945863e03af0943acd99fe446a71fc1
soumya-sunkad <soumyasunkad06@gmail.com>
2025-10-18 11:36:17 +0530

    save

--- file1.py
+++ file1.py
@@ -4,4 +4,5 @@ print("The temperature in degree celsius:",temp)
 print("Converting degree celsius to the fahrenheit:",fahrenheit)
 degree_celsius=(fahrenheit-32)*(5/9)
 print("Fahrenheit to degree celsius:",degree_celsius)
-
+print("Today's temperature is Normal and temperature is:",temp)
+print(f"temperature is {temp} ,Fahrenheit is {fahrenheit}.")
